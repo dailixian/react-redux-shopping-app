ac61e9e885d3ca7c6fb4e51e39c4c3588c44e4e2
Lixian Dai <lixian.dai@aptos.com>
2023-03-14 12:23:33 -0400

    local storage to allow client to refresh page

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,7 +2,15 @@ import { applyMiddleware, legacy_createStore as createStore } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
 
-
+const localStateName = "reduxState"
 export type RootStoreType = ReturnType<typeof store.getState>;
-const store = createStore(rootReducer, applyMiddleware(thunk));
+
+const savedState = localStorage.getItem(localStateName);
+const preState = savedState ? JSON.parse(savedState) : {};
+const store = createStore(rootReducer, preState, applyMiddleware(thunk));
+window.store = store;
+//store keep updating local storage with store state
+store.subscribe(() => {
+    localStorage.setItem(localStateName, JSON.stringify(store.getState()));
+})
 export default store;
